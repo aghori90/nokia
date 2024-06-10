ebd731e5cd8770f387ee286aaf9c16d09ec71eb8
ttusharamber <ttusharamber@gmail.com>
2024-06-10 23:44:47 +0530

    dev

---
+++ lib/ui_helper/util.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+TextStyle mTextStyle11(
+    {Color textColor = Colors.purple,
+    double fontSize = 20,
+    FontWeight fontWeight = FontWeight.bold}) {
+  return TextStyle(
+    fontSize: 15,
+    color: textColor,
+    fontWeight: FontWeight.bold,
+  );
+}
